06b1f6a9c356b1dc50a497b0c3655b3b76e21118
TheoCollignon <theo.collignon2506@gmail.com>
2019-10-19 13:56:59 +0200

    first commit

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { CounterService } from '../counter.service';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
+})
+export class CounterComponent implements OnInit {
+
+  title = "compteur 1"
+  
+  @Input() position;
+
+  constructor(public counterService: CounterService) { }
+
+  ngOnInit() {
+  }
+
+  increment() {
+    this.counterService.increment(this.position);
+  }
+
+}
