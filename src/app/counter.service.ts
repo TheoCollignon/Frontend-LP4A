34f1a383b2bed12f58bebcb2125f767b0901eed4
TheoCollignon <theo.collignon2506@gmail.com>
2019-11-07 22:16:18 +0100

    incrémentation des compteurs

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -7,25 +7,27 @@ import { Counter } from './counter';
   providedIn: 'root'
 })
 export class CounterService {
-  public initialValue = [12, 5, 78];
-
-  constructor(private httpClient: HttpClient) { }
-
-  reset() {
+  public initialValue= [12,6,78];
+  constructor(private httpClient :HttpClient) { }
+  url = 'https://lp4asgadot.herokuapp.com/counters/'
+  
+  reset(){
     this.initialValue=[0,0,0];
   }
-
-  increment(position: number): number {
+  /* increment(position: number) : number{
     this.initialValue[position]++;
     return this.initialValue[position];
+  }*/
+  increment(id:number): Observable<Counter>{
+    return this.httpClient.patch<Counter>(this.url + id + '.json', {});
   }
 
-  decrement(position: number): number {
+  decrement(position: number): number{
     this.initialValue[position]--;
     return this.initialValue[position];
   }
 
-  getCounterValue(id: number): Observable<Counter> {
-    return this.httpClient.get<Counter>("https://lp4asgadot.herokuapp.com/counters/"+id+".json")
+  getCounterValue(id :number) : Observable<Counter>{
+    return this.httpClient.get<Counter>("https://lp4asgadot.herokuapp.com/counters/"+id+".json");
   }
 }
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CounterService } from '../counter.service';
-import { Observable } from 'rxjs';
 import { Counter } from '../counter';
 
 @Component({
@@ -9,25 +8,33 @@ import { Counter } from '../counter';
   styleUrls: ['./counter.component.css']
 })
 export class CounterComponent implements OnInit {
-  
-  @Input() position: number;
+  @Input() position : number;
 
   value: Counter;
-
-  constructor(public counterService: CounterService) {}
+  constructor(public counterService: CounterService) { }
 
   ngOnInit() {
-    console.log("call counter")
-    this.counterService.getCounterValue(this.position)
-          .subscribe(counter => this.value =  counter)
+    if (this.position == 1){
+      this.counterService.getCounterValue(47)
+      .subscribe(counter => this.value =  counter)
+    }
+    if (this.position == 2){
+      this.counterService.getCounterValue(48)
+      .subscribe(counter => this.value =  counter)
+    }
+    if (this.position == 3){
+      this.counterService.getCounterValue(51)
+      .subscribe(counter => this.value =  counter)
+    }
   }
-
-  increment() {
-    this.counterService.increment(this.position);
+  increment(){
+    //this.counterService.increment(this.position);
+        if(this.position == 1) this.counterService.increment(47).subscribe(counter => this.value = counter);
+        if(this.position == 2) this.counterService.increment(48).subscribe(counter => this.value = counter);
+        if(this.position == 3) this.counterService.increment(51).subscribe(counter => this.value = counter);
   }
-
-  decrement() {
+  decrement(){
     this.counterService.decrement(this.position);
   }
-
+  
 }
