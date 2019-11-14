7615bc47d1ebc62acaa295e3e8fbd2a9499f8859
TheoCollignon <theo.collignon2506@gmail.com>
2019-11-14 14:39:34 +0100

    authentification / profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth.guard';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: 'profile',
     component: ProfileComponent,
     canActivate: [AuthGuard]
+  },
+  {
+    path: '',
+    component: HomeComponent
   }
 ];
 
